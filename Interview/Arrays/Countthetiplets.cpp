b512926d699f61980e361324bc0c3ace4b6303c9
JoseJuanSE <jojusuarez46@gmail.com>
2020-11-23 08:09:42 -0600

    tiplets correct

--- Interview/Arrays/Countthetiplets.cpp
+++ Interview/Arrays/Countthetiplets.cpp
@@ -1,19 +1,19 @@
 //link:https://practice.geeksforgeeks.org/problems/count-the-triplets4615/1
 int countTriplet(int arr[], int n){
-        sort(arr,arr+n);
-        reverse(arr,arr+n);
-        int ans=0;
-        for(int i=0;i<n-2;i++){
-            int beg=i+1,end=n-1;
-            while(beg<end){
-                if(arr[i] == arr[beg] + arr[end]){
-                    ans++;  
-                }
-                if(arr[beg] + arr[end] < arr[i])
-                    end--;
-                else
-                    beg++;
+    sort(arr,arr+n);
+    reverse(arr,arr+n);
+    int ans=0;
+    for(int i=0;i<n-2;i++){
+        int beg=i+1,end=n-1;
+        while(beg<end){
+            if(arr[i] == arr[beg] + arr[end]){
+                ans++;  
             }
+            if(arr[beg] + arr[end] < arr[i])
+                end--;
+            else
+                beg++;
         }
-        return ans;
-	}
+    }
+    return ans;
+}
