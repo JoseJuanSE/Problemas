1938a40e60fc69b885c542c7312672e381f782bf
JoseJuanSE <jojusuarez46@gmail.com>
2020-12-11 16:04:21 -0600

    B made it better

--- Codeforces Rounds/Round #689 (div. 2)/B.cpp
+++ Codeforces Rounds/Round #689 (div. 2)/B.cpp
@@ -1,85 +1,36 @@
 #include<bits/stdc++.h>
 
 #define endl '\n'
-#define fi first
-#define se second
 #define fast ios_base::sync_with_stdio(0);cin.tie(0);
-#define ALL(v) v.begin(), v.end()
-#define ALLR(v) v.rbegin(), v.rend()
-#define deb cout<<"HOLA\n";
-#define UNIQUE(v) (v).resize(unique(ALL(v))-(v).begin())
-#define forn(i,n) for (int i = 0; i < n; i++)
 #define forr(i,a,b) for (int i = a; i <= b; i++)
-#define bs(v,f) binary_search(ALL(v),f)
-#define lb(v,f) lower_bound(ALL(v),f)
-#define ub(v,f) upper_bound(ALL(v),f)
-#define pb push_back
 
 using namespace std;
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef string str;
-typedef long double ld;
-typedef pair<int, int> ii;
-typedef pair<ii, int> iii;
-typedef vector<int> vi;
-typedef vector<vector<int> > vvi;
-typedef vector<ll> vl;
-typedef vector<ii> vii;
-char mapa[502][502];
-int a1[502][502];
-int a2[502][502];
-int n,m;
-int checktree(int y,int x,int an){
-    if(y>=n or an>a1[y][x])return 1;
-    return checktree(y+1,x,an+2)+1;
+int ma[503][503],n,m,t;
+int f(int i,int j,int cnum){
+    if(i>n or ma[i][j]<cnum)return 0;
+    return f(i+1,j,cnum+2)+1;
 }
-void solve(){    
+void solve(){
+    char x;
     cin>>n>>m;
-    forn(i,n){
-        forn(j,m){
-            cin>>mapa[i][j];
-            a1[i][j]=0;
-            a2[i][j]=0;
-        }
-    }
-    forn(i,n){
-        forn(j,m){
-            if(mapa[i][j]=='*')a1[i][j]++;
-            if(j>0 and mapa[i][j-1]=='*')a1[i][j]+=a1[i][j-1];
-        }
-    }
-    forn(i,n){
-        for(int j=m-1;j>=0;j--){
-            if(mapa[i][j]=='*')a2[i][j]++;
-            if(j<m-1 and mapa[i][j+1]=='*')a2[i][j]+=a2[i][j+1];
-        }
-    }
-    forn(i,n){
-        forn(j,m){
-            if(mapa[i][j]=='*'){
-                bool xd= a1[i][j]-1 and a2[i][j]-1;
-                a1[i][j]=min(a1[i][j],a2[i][j]);
-                if(xd)a1[i][j]++;
-            }
-            cout<<a1[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    int ans = 0;
-    forn(i,n){
-        forn(j,m){
-            if(a1[i][j]){
-                ans += checktree(i,j,1);
-            }
-        }
-    }
+    forr(i,1,n)
+        forr(j,1,m)
+            cin>>x,ma[i][j]=(x=='*');
+    forr(i,1,n)
+        forr(j,2,m)
+            ma[i][j] += ma[i][j] != 0? ma[i][j-1]:0;
+    forr(i,1,n)
+        for(int j=m-1;j>0;j--)
+            ma[i][j] = (ma[i][j]!=0 and ma[i][j+1]>ma[i][j])?ma[i][j+1]:ma[i][j];
+    int ans=0;
+    forr(i,1,n)
+        forr(j,1,m)
+            ans+= ma[i][j]?f(i,j,1):0;
     cout<<ans<<endl;
 }
 int main(){
     fast
-    int t=1;
     cin>>t;
     while(t--)
         solve();
